a39bf6b46e0d7413d00a95f7e33f86fe4cdf3451
dg1223 <alavi1223@hotmail.com>
2023-04-30 14:44:52 -0400

    created phones API

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
 const express = require("express");
+const phones = require("./phones.json");
 const app = express();
 const port = 5000;
 
 app.get("/", (req, res) => {
   res.send("My phone information coming soon");
+});
+
+app.get("/phones", (req, res) => {
+  res.send(phones);
+});
+
+app.get("/phones/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  console.log("I need data for id: ", id);
+  const phone = phones.find((phone) => phone.id === id) || {};
+  res.send(phone);
 });
 
 app.listen(port, () => {
